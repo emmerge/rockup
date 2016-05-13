0edfee7bc5b1f99530c922a0af779e96c800c244
Colin McCloskey <colin.mccloskey@gmail.com>
2016-05-13 00:59:58 -0400

    Init: Turn off upstart and nginx template generation by default

--- bin/rock-init.js
+++ bin/rock-init.js
@@ -7,12 +7,16 @@ var Config = require("../lib/Config");
 
 program
   .arguments("<app> <environment>")
-  .option("--skip-upstart", "Don't include upstart configuration")
-  .option("--skip-nginx", "Don't include nginx configuration")
+  .option("--upstart", "Add upstart configuration template")
+  .option("--nginx", "Add nginx configuration template")
   .action( function(appName, environment) {
     
     console.log("Creating", environment.cyan, "rockup file for", appName.cyan, "app");
     var files = ['rockup','meteor'];  // upstart, nginx
+    if (this.upstart)
+      files.push('upstart');
+    if (this.nginx)
+      files.push('nginx');
     Config.initialize(appName, environment, files);
 
   });
