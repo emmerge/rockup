7c9dbd751ca8b6ffd970c2379bf77d19299784bc
Colin McCloskey <colin.mccloskey@gmail.com>
2016-05-26 16:10:11 -0400

    rock history command
    Command to retrieve a release history from all hosts in the environment.

--- bin/rock.js
+++ bin/rock.js
@@ -167,7 +167,41 @@ program
 program
   .command("history <environment>")
   .alias("hist")
-  .description("Deployment history");
+  .description("Deployment history")
+  .action( function(environment) {
+    var config = _loadLocalConfigFile(environment);
+
+    var numPending = config.hosts.count;
+    var allHosts = config.hosts.map( function(h) { return h.shortName; });
+    var allReleases = {};
+    config.hosts.each( function(host) {
+      host.history( function(err, hostReleases) {
+        if (err) { _endCommandCallback(err); }
+        else { 
+          _.each(hostReleases, function(r) {
+            if (allReleases[r])
+              allReleases[r].push(host.shortName);
+            else
+              allReleases[r] = [host.shortName];
+          });
+        }
+        --numPending;
+        if (numPending <= 0) {
+          //console.log("Releases:", inspect(allReleases));
+          console.log("Release            Hosts           ".underline.yellow);
+          _.each(allReleases, function(hostNames, releaseName) {
+            var note = "";
+            if (hostNames.length < allHosts.length)
+              note = "* only "+hostNames.join(", ");
+            console.log(releaseName," ",note);
+          });
+          console.log("");
+          process.exit(0);
+        }
+      });
+    });
+
+  });
 
 /** Catch-all for unhandled commands: **/
 program
