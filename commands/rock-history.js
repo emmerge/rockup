b9eb872ad2af848411597b4fdf27b6c6522ccac8
Colin McCloskey <colin.mccloskey@gmail.com>
2016-05-27 01:25:08 -0400

    History Command
    Multiplex results from all hosts, sort final list, indicate proper current release.
    
    Introduce Util.reduceAsync to parallelize async requests across multiple hosts with a reliable final callback. Use for history query.

--- commands/rock-history.js
+++ commands/rock-history.js
@@ -3,6 +3,8 @@
 
 var RockUtil = require('./util');
 var Config = require('../lib/Config');
+var inspect = require('util').inspect;
+var _ = require('underscore');
 
 module.exports = HistoryCommand;
 
@@ -13,37 +15,55 @@ function HistoryCommand (program) {
     .alias("hist")
     .description("Deployment history")
     .action( function(environment) {
-      var config = _loadLocalConfigFile(environment);
+      var config = Config._loadLocalConfigFile(environment);
+      var releaseMap = {};
 
-      var numPending = config.hosts.count;
-      var allHosts = config.hosts.map( function(h) { return h.shortName; });
-      var allReleases = {};
-      config.hosts.each( function(host) {
-        host.history( function(err, hostReleases) {
-          if (err) { _endCommandCallback(err); }
-          else { 
-            _.each(hostReleases, function(r) {
-              if (allReleases[r])
-                allReleases[r].push(host.shortName);
-              else
-                allReleases[r] = [host.shortName];
-            });
-          }
-          --numPending;
-          if (numPending <= 0) {
-            //console.log("Releases:", inspect(allReleases));
-            console.log("Release            Hosts           ".underline.yellow);
-            _.each(allReleases, function(hostNames, releaseName) {
-              var note = "";
-              if (hostNames.length < allHosts.length)
-                note = "* only "+hostNames.join(", ");
-              console.log(releaseName," ",note);
-            });
-            console.log("");
-            process.exit(0);
-          }
-        });
+      var ops = config.hosts.map( function(host) {
+        return function (memo, cb) {
+          host.history( function (err, releases) {    // releases: {current: 'releaseName', list: ['name1','name2','name3']}
+            if (err) { cb(err); }
+            else {
+              releases = _.map(releases.list, function(r) { 
+                if (r == releases.current)
+                  return r+" <=".cyan.bold;
+                else
+                  return r;
+              });
+              memo[host.shortName] = releases;
+              cb();
+            }
+          });
+        };
       });
+
+      // Receives map of {hostName: [releases]}
+      function allHostsComplete ( memo ) {
+        _.each(memo, function(releases, hostName) {
+          _.each(releases, function(r) {
+            if (releaseMap[r])
+              releaseMap[r].push(hostName);
+            else
+              releaseMap[r] = [hostName];
+          });
+        });
+
+        var finalMap = {};
+        _.chain(releaseMap).keys().sort().reverse().each(function(k) { finalMap[k] = releaseMap[k]; });
+        console.log("Release            \tHosts           ".underline.yellow);
+        _.each(finalMap, function(hostNames, releaseName) {
+          var note = "";
+          if (hostNames.length < config.hosts.count)
+            note = "* only "+hostNames.join(", ");
+          console.log(releaseName," \t",note);
+        });
+        console.log("");
+        process.exit(0);
+      }
+
+      ops.unshift({}); // memo
+      ops.push(allHostsComplete);
+
+      RockUtil.reduceAsync.apply(this, ops);
 
     });
 
