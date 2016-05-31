d6aa3680fcec0b57601c46c01fad65f561ad6dfb
Colin McCloskey <colin.mccloskey@gmail.com>
2016-05-31 16:30:09 -0400

    Status Command: Multi-host service inspection
    Return and summarize status details for every service on every host in the environment.

--- commands/rock-status.js
+++ commands/rock-status.js
@@ -1,6 +1,9 @@
 // RockUp
 // Commands-Status -- Load stopped/running state for services
 
+var inspect = require('util').inspect;
+var _ = require('underscore');
+
 var RockUtil = require('./util');
 var Config = require('../lib/Config');
 
@@ -11,20 +14,47 @@ function StatusCommand (program) {
   program
     .command("status <environment>")
     .description("Display status for running/stopped services")
-    .option("--host <name>", "A specific host to target")
-    .option("--service <name>", "A specific service to target")
     .action( function(env, cliOptions) {
       var config = Config._loadLocalConfigFile(env);
-      if ( cliOptions.host ) {
-        var host = config.hosts.get( cliOptions.host );
-        console.log("Working on host:", host.name);
-        host.services.run.status( function(results) {
-          console.log("Results:\n", inspect(results, {colors:true, depth:null}));
-          process.exit(0);
-        });
 
-      } else {
-        console.log("Can't do it for everyone, yet. Pass a --host");
+      var ops = config.hosts.map( function(host) {
+        return function (memo, cb) {
+          host.status( function(err, status, map) {
+            if (err) { 
+              cb(err); 
+            }
+            else {
+              memo[host.name] = [status, map];
+              cb();
+            }
+          }); // host.status
+        }; // return
+      }); // map
+
+      // Receives map of hostname: [status string, serviceMap{}]
+      function allHostsComplete ( statusMap ) {
+        // console.log("Host Status Map:\n", inspect(statusMap, {colors:true, depth:null}));
+        console.log("");
+        _.each( statusMap, function(statusDetails, hostName) {
+          var status = statusDetails[0];
+          var serviceMap = statusDetails[1];
+          console.log(hostName.bold, "\t", _colorizeStatus(status), "\t", inspect(serviceMap).replace("\n", " ") );
+        });
+        console.log("");
+        process.exit(0);
+      }
+
+      ops.unshift({}); // memo
+      ops.push(allHostsComplete);
+
+      console.log("Querying hosts for service status...");
+
+      RockUtil.reduceAsync.apply(this, ops);
+
+      function _colorizeStatus(s) {
+        if (s == 'running') return s.green;
+        else if (s == 'stopped') return s.red;
+        else return s.blue;
       }
 
     });
