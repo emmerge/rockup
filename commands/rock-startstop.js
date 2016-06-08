fc1dab1fc1f780ce6d5f25ef8626e5cdd414dbd9
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-08 17:43:52 -0400

    LifeCycle start, stop, restart CLI complete

--- commands/rock-startstop.js
+++ commands/rock-startstop.js
@@ -1,10 +1,11 @@
 // RockUp
 // Commands-StartStop -- Cycle service state within target environment
 
-var RockUtil = require('./util');
 var Config = require('../lib/Config');
 
 var _ = require('underscore');
+var inspect = require('util').inspect;
+var Spinner = require('clui').Spinner;
 
 module.exports = StartStopCommand;
 
@@ -14,12 +15,39 @@ function StartStopCommand (program) {
     program
       .command(command+" <environment>")
       .option("--host <name>", "The specific host to target")
-      .option("--service <name>", "The specific service to target")
       .action( function(env, cliOptions) {
         var config = Config._loadLocalConfigFile(env);
-        config.hosts.each( function(host) {
-          host.services.run[command]( RockUtil._endCommandCallback(command) );
+        var hosts;
+        if (cliOptions && cliOptions.host) 
+          hosts = [config.hosts.get(cliOptions.host)];
+        else
+          hosts = config.hosts.list;
+
+        var gerund = { 'start': 'Starting', 'stop': 'Stopping', 'restart': 'Restarting'}[command];
+        var spinner = new Spinner(gerund+' services on '+hosts.length+' hosts...');
+        spinner.start();
+        
+
+        var remain = hosts.length;
+        var hostMap = {};
+        _.each(hosts, function(host) {
+          host[command]( function(result) {
+            --remain;
+            hostMap[host.name] = result;
+
+            if (remain === 0) {
+              spinner.stop();
+              console.log("");
+              _.each(hostMap, function(result, hostName) {
+                console.log(hostName.bold, "\t", inspect(result, {colors:true}).replace("\n", " ") );
+              });
+              console.log("");
+              process.exit(0);
+            }
+          });
         });
+
+
       });
   });
 
