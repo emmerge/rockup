26d70196df799ad85817c58e9fdee2e70916e51c
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-10 16:41:27 -0400

    Reconfigure and Deploy optimizations

--- commands/rock-reconfig.js
+++ commands/rock-reconfig.js
@@ -1,8 +1,8 @@
 // RockUp
 // Commands-Reconfig -- Push configuration only and restart services
 
-var RockUtil = require('./util');
 var Config = require('../lib/Config');
+var Deploy = require('../lib/Deploy');
 
 module.exports = ReconfigCommand;
 
@@ -10,7 +10,29 @@ function ReconfigCommand (program) {
 
   program
     .command("reconfig <environment>")
-    .description("Push configuration only and restart");
+    .option("--host <name>", "Reconfigure a specific host")
+    .description("Change live configuration and restart")
+    .action( function(environment, cliOptions) {
+        var config = Config._loadLocalConfigFile(environment);
+        cliOptions = cliOptions || {};
+        var options = {};
+        if ( cliOptions.host )
+          options.hosts = [cliOptions.host];            // Target a single host
+        
+        var deployment = new Deploy(config, options);
+        deployment.reconfigure( function(err, results) {
+          console.log("\nReconfigure complete", results.successful.join(', '));
+          console.log( inspect(results, {colors:true, depth:null}) );
+          if (results.failed.length === 0) {
+            console.log("Reconfiguration succeeded!\n".green.bold);
+            process.exit(0);
+          } else {
+            console.log("Failed to reconfigure", results.failed.join(', '));
+            console.log("Reconfiguration failed!\n".red.bold);
+            process.exit(1);
+          }
+        });
+    });
 
   return program;
 }
