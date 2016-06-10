b20344ffb66fef174ce7a189f2ee04b286d0b524
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-10 00:24:15 -0400

    Config.initialize: Template for rockup.json, better defaults in CLI

--- commands/rock-init.js
+++ commands/rock-init.js
@@ -7,17 +7,24 @@ module.exports = InitCommand;
 
 function InitCommand (program) {
   program
-    .command("init <environment>")
-    .description("Initialize a local configuration")
+    .command("init <environment> <appName>")
+    .option("-p, --path <path>", "Path to app relative to ./deploy")
     // .option("--upstart", "Add upstart configuration template")
     // .option("--nginx", "Add nginx configuration template")
-    .action( function(env, options) {
-      console.log("Creating local rockup configs for ", env.cyan, "environment");
+    .description("Initialize a local configuration.")
+    .on("--help", function(){
+      console.log("  Examples:\n");
+      console.log("    $ rock init production myapp                  # Basic myapp production config");
+      console.log("    $ rock init staging MyApp --path /src/myapp   # Config with path to app dir\n");
+    })
+    .action( function(environment, appName, cliOptions) {
+      console.log("Creating local rockup configs for", environment.cyan, "environment");
       var files = ['rockup','meteor'];  // TODO: upstart, nginx
-      // if (options.upstart) files.push('upstart');
-      // if (options.nginx) files.push('nginx');
-      Config.initialize(env, files);
-      console.log("");
+      // if (cliOptions.upstart) files.push('upstart');
+      // if (cliOptions.nginx) files.push('nginx');
+      var appPath = cliOptions.path;
+      Config.initialize(environment, files, appName, appPath);
+      console.log();
     });
 
   return program;
