f95f909ceba9af84c1bb3cdfa141e3664aca21da
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-10 11:38:44 -0400

    History: Report fail-to-query hosts

--- commands/rock-history.js
+++ commands/rock-history.js
@@ -20,19 +20,20 @@ function HistoryCommand (program) {
 
       var hosts = cliOptions.host ? [config.hosts.get(cliOptions.host)] : config.hosts.list;
       var numHosts = hosts.length;
+      var errorHosts = [];
 
       var spinner = new Spinner('Pulling history from '+numHosts+' host(s)...');
       spinner.start();
-
-      var releaseMap = {};
 
       var ops = _.map(hosts, function(host) {
         return function (memo, cb) {
           host.history( function (err, hostReleases) {    // releases: {current: 'releaseName', list: ['name1','name2','name3']}
             --numHosts;
             spinner.message('Pulling history from '+numHosts+' host(s)...');
-            if (err) { cb(err); }
-            else {
+            if (err) { 
+              errorHosts.push(host.name);
+              cb(err); 
+            } else {
               _.each(hostReleases.list, function(releaseName) {
                 var rInfo = { name: releaseName, host: host.name };
                 if (releaseName == hostReleases.current) { rInfo.current = true; }
@@ -48,6 +49,7 @@ function HistoryCommand (program) {
       // duplicates. Items in array formatted like:
       // [ { name: 'r20160610_631983', current: true, host: 'app1.com' }, ... ]
       function allHostsComplete ( allReleases ) {
+        allReleases = _(allReleases).sortBy('name');
         var releaseMap = {};
         _.each(allReleases, function(rInfo) {
           if ( releaseMap[rInfo.name] ) {
@@ -88,8 +90,12 @@ function HistoryCommand (program) {
             .fill()
             .output();
         });
-
         console.log("  * currents highlighted".dim, "\n");
+
+        if (errorHosts.length > 0) {
+          console.log("  => Failure:".red.bold, "Unable to query hosts:", errorHosts.join(', '), "\n");
+        }
+
         process.exit(0);
       }
 
