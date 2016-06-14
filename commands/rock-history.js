50df7aca609c9b48e98563bd8d7f11dc1e97e231
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-13 21:14:15 -0400

    History CLI: Convert to npm aysnc, better parallelize, add --all

--- commands/rock-history.js
+++ commands/rock-history.js
@@ -1,7 +1,7 @@
 // RockUp
 // Commands-History -- Retrieve list of upstream deployment history
 
-var reduceAsync = require("../lib/Async").reduce;
+var async = require('async');
 var Config = require('../lib/Config');
 
 var Spinner = CLUI.Spinner;
@@ -15,8 +15,8 @@ function HistoryCommand (program) {
     .command("history <environment>")
     .option("--host <name>", "Limit history lookup to single host")
     .option("--all", "Only list releases available on all hosts")
-    .option("--current", "Return only the name of the current release")
-    .option("--previous", "Return only the name of the release prior to current")
+    // .option("--current", "Return only the name of the current release")
+    // .option("--previous", "Return only the name of the release prior to current")
     .description("View deployment history")
     .on("--help", function(){
       console.log("  Examples:\n");
@@ -31,97 +31,138 @@ function HistoryCommand (program) {
       console.log("    conjunction with --host");
       console.log();
     })
-
-    .action( function(environment, cliOptions) {
-      var config = Config._loadLocalConfigFile(environment);
-
-      var hosts = cliOptions.host ? [config.hosts.get(cliOptions.host)] : config.hosts.list;
-      var numHosts = hosts.length;
-      var errorHosts = [];
-
-      var spinner = new Spinner('Pulling history from '+numHosts+' host(s)...');
-      spinner.start();
-
-      var ops = _.map(hosts, function(host) {
-        return function (memo, cb) {
-          host.history( function (err, hostReleases) {    // releases: {current: 'releaseName', list: ['name1','name2','name3']}
-            --numHosts;
-            spinner.message('Pulling history from '+numHosts+' host(s)...');
-            if (err) { 
-              errorHosts.push(host.name);
-              cb(err); 
-            } else {
-              _.each(hostReleases.list, function(releaseName) {
-                var rInfo = { name: releaseName, host: host.name };
-                if (releaseName == hostReleases.current) { rInfo.current = true; }
-                memo.push( rInfo );
-              });
-              cb();
-            }
-          });
-        };
-      });
-
-      // Completion function receives array releases that will contain
-      // duplicates. Items in array formatted like:
-      // [ { name: 'r20160610_631983', current: true, host: 'app1.com' }, ... ]
-      function allHostsComplete ( allReleases ) {
-        allReleases = _(allReleases).sortBy('name').reverse();
-        var releaseMap = {};
-        _.each(allReleases, function(rInfo) {
-          if ( releaseMap[rInfo.name] ) {
-            // Release duplicate. Merge:
-            releaseMap[rInfo.name].hosts.push( config.hosts.get(rInfo.host).shortName );
-            releaseMap[rInfo.name].hosts = releaseMap[rInfo.name].hosts.sort();
-            if (rInfo.current) releaseMap[rInfo.name].current = rInfo.current;
-          } else {
-            // Unseen release. Insert:
-            rInfo.hosts = [ config.hosts.get(rInfo.host).shortName ];
-            delete rInfo.host;
-            releaseMap[rInfo.name] = _(rInfo).omit('name');
-          }
-        });
-
-        spinner.stop();
-        var headers = new Line()
-          .padding(2)
-          .column("Release".yellow.bold.underline, 24)
-          .column("Hosts".yellow.bold.underline, 40)
-          .fill()
-          .output();
-
-        _.each(releaseMap, function(rInfo, releaseName) {
-          var displayName = rInfo.current ? releaseName.cyan.bold : releaseName;
-          var hostsNote = "All  ("+rInfo.hosts.length+")";
-
-          if ( hosts.length > rInfo.hosts.length ) {
-            // At least 1 env host does not have this release
-            displayName = displayName.italic;
-            hostsNote = "Some ("+rInfo.hosts.length+") "+rInfo.hosts.join(", ").dim;
-          }
-
-          var line = new Line()
-            .padding(2)
-            .column(displayName, 24)
-            .column(hostsNote, 40)
-            .fill()
-            .output();
-        });
-        console.log("  * currents highlighted".dim, "\n");
-
-        if (errorHosts.length > 0) {
-          console.log("  => Failure:".red.bold, "Unable to query hosts:", errorHosts.join(', '), "\n");
-        }
-
-        process.exit(0);
-      }
-
-      ops.unshift([]); // memo
-      ops.push(allHostsComplete);
-
-      reduceAsync.apply(this, ops);
-
-    });
+    .action( _gatherHistory );
 
   return program;
+}
+
+
+/**
+ * Perform the history operation.
+ *
+ **/
+function _gatherHistory(environment, cliOptions) {
+  var config = Config._loadLocalConfigFile(environment);
+  var hosts = _getHosts(config, cliOptions);
+  var numHosts = hosts.length;
+
+  var spinner = new Spinner("Pulling history from "+(numHosts==1 ? "1 host" : numHosts+" hosts")+"...");
+  spinner.start();
+
+  // Gather an array of
+  var calls = {};
+  _(hosts).each( function(host) {
+    var f = function(cb) {
+      host.history( function(err, hostReleases) {
+        --numHosts;
+        spinner.message("Pulling history from "+(numHosts==1 ? "1 host" : numHosts+" hosts")+"...");
+        if (err) { 
+          cb(err); 
+        } else {
+          var releases = _.map(hostReleases.list, function(releaseName) {
+            var rInfo = { name: releaseName };
+            if (releaseName == hostReleases.current) { rInfo.current = true; }
+            return rInfo;
+          });
+          cb(null, releases);
+        }
+      });
+    };
+    calls[host.name] = f;
+  });
+
+  // Completion function receives an Object with hostname keys and lists of
+  // releases as values. Each release: name: 'r20160610', current: true }
+  function _allComplete ( err, hostMap ) {
+    spinner.stop();
+    if (err) {
+      console.log("  => Failure:".red.bold, "Unable to load history:", err, "\n");
+      process.exit(1);
+    }
+
+    var releaseList;
+    if (cliOptions.all)
+      releaseList = _.sortBy( _onlyOnAllHosts(hostMap, _(hosts).pluck('name')), 'name' ).reverse();
+    else
+      releaseList = _.sortBy( _onAnyHost(hostMap), 'name' ).reverse();
+
+    var headers = new Line()
+      .padding(2)
+      .column("Release".yellow.bold.underline, 24)
+      .column("Hosts".yellow.bold.underline, 40)
+      .fill()
+      .output();
+
+    _.each(releaseList, function(rInfo) {
+      var releaseName = rInfo.name;
+      var displayName = rInfo.current ? releaseName.cyan.bold : releaseName;
+      var hostsNote = "All  ("+rInfo.hosts.length+")";
+      if ( hosts.length > rInfo.hosts.length ) {
+        // At least 1 env host does not have this release
+        displayName = displayName.italic;
+        var hostShortNames = _(rInfo.hosts).map(function(hn) { return hn.split('.')[0]; });
+        hostsNote = "Some ("+rInfo.hosts.length+") "+hostShortNames.join(", ").dim;
+      }
+      var line = new Line()
+        .padding(2)
+        .column(displayName, 24)
+        .column(hostsNote, 40)
+        .fill()
+        .output();
+    });
+
+    console.log();
+    process.exit(0);
+  }
+
+  async.parallel(calls, _allComplete);
+}
+
+/**
+ * Return a list of Hosts that will be used in this invocation. Can
+ * be altered by user specifying a host limitation or using host-
+ * specifying options on the command line.
+ *
+ * @params {Config} config      The environment configuration in use
+ * @params {Object} cliOptions  The commander options 
+ * @returns {Array Host} List of target hosts
+ **/
+function _getHosts(config, cliOptions) {
+  return cliOptions.host ? [config.hosts.get(cliOptions.host)] : config.hosts.list;
+}
+
+/**
+ * Given an Object of { hostName: [ releases ] }, return a list of
+ * releases that are present on _every_ host.
+ *
+ * @param {Object} hostMap      Map of host name => array of releases
+ * @param {Array} hostNames     Array of all present host names
+ * @returns {Array} Releases present on all hosts
+ **/
+function _onlyOnAllHosts(hostMap, hostNames) {
+  var allReleases = _onAnyHost(hostMap);
+  return _(allReleases).reject(function(r) {
+    return r.hosts.length < hostNames.length;
+  });
+}
+
+/**
+ * Given an Object of { hostName: [ releases ] }, return a list of 
+ * every release, even those only on a subset of hosts.
+ *
+ * @param {Object} hostMap      Map of host name => array of releases
+ * @returns {Array} List of releases
+ **/
+function _onAnyHost(hostMap) {
+  var allReleases = [];
+  _(hostMap).each( function(releases, hostName) {
+    _(releases).each( function(r) {
+      var exist = _(allReleases).findWhere({name: r.name});
+      if (exist)
+        exist.hosts = _.union(exist.hosts, [hostName]);
+      else
+        allReleases.push( _(r).extend({hosts: [hostName]}) );
+    });
+  });
+  return allReleases;
 }
