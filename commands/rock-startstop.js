f33f5739cc028199bbd7e45a5fba0609de4288a4
Colin McCloskey <colin.mccloskey@gmail.com>
2016-06-15 14:47:01 -0400

    LifeCycle invocation on CLI

--- commands/rock-startstop.js
+++ commands/rock-startstop.js
@@ -1,47 +1,72 @@
 // RockUp
 // Commands-StartStop -- Cycle service state within target environment
 
+var _getHosts = require('../commands/util')._getHosts;
 var Config = require('../lib/Config');
 var Spinner = CLUI.Spinner;
+
+var async = require('async');
 
 module.exports = StartStopCommand;
 
 function StartStopCommand (program) {
 
-  _.each(["start", "stop", "restart"], function(command) {
+  _.each(["start", "stop", "restart"], function(actionName) {
     program
-      .command(command+" <environment>")
-      .description("On host, "+command+" services running this app")
-      .option("--host <name>", "Specific host to target")
-      .action( function(env, cliOptions) {
-        var config = Config._loadLocalConfigFile(env);
-        var hosts = cliOptions.host ? [config.hosts.get(cliOptions.host)] : config.hosts.list;
-
-        var gerund = { 'start': 'Starting', 'stop': 'Stopping', 'restart': 'Restarting'}[command];
-        var spinner = new Spinner(gerund+' services on '+hosts.length+' host(s)...');
-        spinner.start();
-
-        var remain = hosts.length;
-        var hostMap = {};
-        _.each(hosts, function(host) {
-          host[command]( function(err, result) {
-            --remain;
-            spinner.message(gerund+' services on '+remain+' host(s)...');
-            hostMap[host.name] = result;
-            if (remain === 0) {
-              spinner.stop();
-              console.log("");
-              _.each(hostMap, function(result, hostName) {
-                console.log(hostName.bold, "\t", inspect(result, {colors:true}).replace("\n", " ") );
-              });
-              console.log("");
-              process.exit(0);
-            }
-          });
-        });
-
+      .command(actionName+" <environment>")
+      .description("On host, "+actionName+" services running this app")
+      .option("--host <name>", "Target a single host from environment")
+      // .option("--service <name>", "Specify individual service to "+actionName)
+      .action( function(environment, cliOptions) {
+        _performAction(actionName, environment, cliOptions);
       });
+
   });
 
   return program;
 }
+
+/**
+ * Perform the start/stop/restart operation.
+ *
+ * @params {String} actionName        The name of the action we're taking (e.g., 'start')
+ * @params {String} environment       The RockUp environment name
+ * @params {Object} cliOptions        The Commander CLI options
+ **/
+function _performAction ( actionName, environment, cliOptions ) {
+  var config = Config._loadLocalConfigFile(environment);
+  var hosts = _getHosts(config, cliOptions);
+  var numHosts = hosts.length;
+  var gerund = { 'start': 'Starting', 'stop': 'Stopping', 'restart': 'Restarting'}[actionName];
+  
+  var spinner = new Spinner(gerund+" services on "+(numHosts==1 ? "1 host" : numHosts+" hosts")+"...");
+  spinner.start();
+
+  var calls = {};
+  _(hosts).each( function(host) {
+    var f = function(cb) {
+      host[actionName]( function(err, result) {
+        --numHosts;
+        spinner.message(gerund+" services on "+(numHosts==1 ? "1 host" : numHosts+" hosts")+"...");
+        cb(err, result);
+      });
+    };
+    calls[host.name] = f;
+  });
+
+  function _allComplete (err, hostMap) {
+    spinner.stop();
+    if (err) {
+      console.log("  => Failure:".red.bold, "Unable to perform "+actionName+" action :", err, "\n");
+      process.exit(1);
+    }
+    _.each(hostMap, function(result, hostName) {
+      console.log(hostName.bold, "\t", inspect(result, {colors:true}).replace("\n", " ") );
+    });
+    console.log();
+  }
+
+  async.parallel( calls, _allComplete );
+
+}
+
